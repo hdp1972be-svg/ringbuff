aec3ba4f6b9ddf6226afc39a50b54d7674fcb353
hdp1972be-svg <hdp1972be@gmail.com>
2026-09-13 05:41:10 +0200

    Add atomic fetch helpers for notification waiters

--- include/rb_port.h
+++ include/rb_port.h
@@ -1,14 +1,11 @@
 /* SPDX-License-Identifier: MIT */
 /* Copyright (c) 2026 H. De Pauw */
-
 #ifndef RB_PORT_H
 #define RB_PORT_H
-
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
 #include "rb_config.h"
-
 #if RB_USE_ATOMICS
 #  include <stdatomic.h>
    typedef _Atomic uint32_t rb_atomic_u32;
@@ -16,6 +13,8 @@
 #  define RB_ATOMIC_LOAD_RLX(p)    atomic_load_explicit((p), memory_order_relaxed)
 #  define RB_ATOMIC_STORE_REL(p,v) atomic_store_explicit((p),(v),memory_order_release)
 #  define RB_ATOMIC_STORE_RLX(p,v) atomic_store_explicit((p),(v),memory_order_relaxed)
+#  define RB_ATOMIC_FETCH_ADD(p,v) atomic_fetch_add_explicit((p),(v),memory_order_relaxed)
+#  define RB_ATOMIC_FETCH_SUB(p,v) atomic_fetch_sub_explicit((p),(v),memory_order_relaxed)
 #else
    typedef volatile uint32_t rb_atomic_u32;
 #  if RB_SINGLE_THREADED
@@ -23,23 +22,24 @@
 #    define RB_ATOMIC_LOAD_RLX(p)    (*(p))
 #    define RB_ATOMIC_STORE_REL(p,v) do { *(p)=(v); } while (0)
 #    define RB_ATOMIC_STORE_RLX(p,v) do { *(p)=(v); } while (0)
+#    define RB_ATOMIC_FETCH_ADD(p,v) (*(p) += (v), *(p) - (v))
+#    define RB_ATOMIC_FETCH_SUB(p,v) (*(p) -= (v), *(p) + (v))
 #  elif defined(__GNUC__) || defined(__clang__)
 #    define RB_ATOMIC_LOAD_ACQ(p)    (*(p))
 #    define RB_ATOMIC_LOAD_RLX(p)    (*(p))
 #    define RB_ATOMIC_STORE_REL(p,v) do { __asm__ volatile("" ::: "memory"); *(p)=(v); } while (0)
 #    define RB_ATOMIC_STORE_RLX(p,v) do { *(p)=(v); } while (0)
+#    define RB_ATOMIC_FETCH_ADD(p,v) __atomic_fetch_add((p),(v),__ATOMIC_RELAXED)
+#    define RB_ATOMIC_FETCH_SUB(p,v) __atomic_fetch_sub((p),(v),__ATOMIC_RELAXED)
 #  else
 #    error "No atomic implementation available for this target"
 #  endif
 #endif
-
 #define RB_ALIGN_UP(x,a) (((x) + ((size_t)((a)-1u))) & ~((size_t)((a)-1u)))
 #define RB_IS_POW2(x)    ((x) != 0u && (((x) & ((x)-1u)) == 0u))
-
 #if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201112L
 #  define RB_ALIGNAS(n) _Alignas(n)
 #else
 #  define RB_ALIGNAS(n) __attribute__((aligned(n)))
 #endif
-
 #endif /* RB_PORT_H */
